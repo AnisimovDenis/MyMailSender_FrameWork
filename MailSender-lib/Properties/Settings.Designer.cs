04b19d66deb96aea32560cd3c839d542a5b2807d
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-03 22:47:58 +0300

    Добавил подключение к БД, а так же подключил данные из БД к ListBox

--- MailSender-lib/Properties/Settings.Designer.cs
+++ MailSender-lib/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MyMailSender.Properties {
+namespace MailSender_lib.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
