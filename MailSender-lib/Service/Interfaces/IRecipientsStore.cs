733751537818de8cdb126f6fd09ea22639e49011
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-09 19:13:58 +0300

    Добавил интерфейсы

--- MailSender-lib/Service/Interfaces/IRecipientsStore.cs
+++ MailSender-lib/Service/Interfaces/IRecipientsStore.cs
@@ -7,12 +7,5 @@ using System.Threading.Tasks;
 
 namespace MailSender_lib.Service.Interfaces
 {
-    public interface IRecipientsStore
-    {
-        IEnumerable<Recipient> Get();
-
-        void Edit(int id, Recipient Recipient);
-
-        void SaveChanges();
-    }
+    public interface IRecipientsStore : IDataStore<Recipient> { }
 }
