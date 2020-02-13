269389f5f94235385a2073cb4d7a698a8dc4874d
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-13 22:22:03 +0300

    Написал методы

---
+++ MyThreadTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MyThreadTest
+{
+    class Program
+    {
+        static Mutex resetEvent = new Mutex();
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        public static void Factorial()
+        {
+            resetEvent.WaitOne();
+            Console.Write("Введите число факториала: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int length = num;
+            for (int i = 1; i < length; i++)
+            {
+                num *= i;
+            }
+            Console.WriteLine("Результат факториала = {0}", num);
+            resetEvent.ReleaseMutex();
+        }
+
+
+        public static void Sum()
+        {
+            resetEvent.WaitOne();
+            Console.Write("Введите число, до которого производить сумму: ");
+            int length = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 0; i <= length; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine("Результат = {0}", result);
+            resetEvent.ReleaseMutex();
+        }
+    }
+}
