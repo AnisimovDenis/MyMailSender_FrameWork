9cdf04819a00a623ffd137bb8d8d7c0dfcfb64c4
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-17 20:55:56 +0300

    Реализовал класс

--- MyTaskTest/MyMatrix.cs
+++ MyTaskTest/MyMatrix.cs
@@ -8,5 +8,38 @@ namespace MyTaskTest
 {
     class MyMatrix
     {
+        static Random rnd = new Random();
+
+        public static void RandMatrix(ref int[,] mass)
+        {
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = rnd.Next(0, 9);
+                }
+            }
+        }
+
+        public static void IncreaseMatrix(ref int[,] mass, int increase)
+        {
+            int[,] newMass = new int[mass.GetLength(0) * increase, mass.GetLength(1) * increase];
+
+            RandMatrix(ref newMass);
+
+            mass = newMass;
+        }
+
+        public static void PrintMatrix(int[,] mass)
+        {
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
