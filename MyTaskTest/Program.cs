ae94cd25f4b9fd9db927bf136f1643b87fdf7e41
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-17 20:56:55 +0300

    Реализовал работку задач

--- MyTaskTest/Program.cs
+++ MyTaskTest/Program.cs
@@ -10,6 +10,43 @@ namespace MyTaskTest
     {
         static void Main(string[] args)
         {
+            int[,] firstArray = new int[5, 5];
+            int[,] secondArray = new int[10, 10];
+
+            MyMatrix.RandMatrix(ref firstArray);
+
+            MyMatrix.PrintMatrix(firstArray);
+
+
+            Console.WriteLine();
+
+
+            MyMatrix.RandMatrix(ref secondArray);
+
+            MyMatrix.PrintMatrix(secondArray);
+
+
+
+            Console.WriteLine();
+
+
+
+            Task taskFirstArray = new Task(() => MyMatrix.IncreaseMatrix(ref firstArray, 2));
+            Task taskSecondArray = new Task(() => MyMatrix.IncreaseMatrix(ref firstArray, 2));
+            taskSecondArray.Start();
+            taskFirstArray.Start();
+            taskSecondArray.Wait();
+            taskFirstArray.Wait();
+
+
+
+            MyMatrix.PrintMatrix(firstArray);
+
+            Console.WriteLine();
+
+            MyMatrix.PrintMatrix(secondArray);
+
+            Console.ReadKey();
         }
     }
 }
