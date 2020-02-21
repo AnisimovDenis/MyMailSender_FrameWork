a217a114441cfc2929f3cea70a6d003b26c89336
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-21 12:20:40 +0300

    Разработал БД

---
+++ MailSender-lib/Data/TestDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender_lib.Data
+{
+    class TestDBContext : DbContext
+    {
+        static TestDBContext() => Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestDBContext, Configuration>());
+
+        public DbSet<Recipient> Recipient { get; set;}
+
+        public TestDBContext() : this("name=DefaultConnection") { }
+
+        public TestDBContext(string ConnectionString) : base(ConnectionString) { }
+    }
+}
