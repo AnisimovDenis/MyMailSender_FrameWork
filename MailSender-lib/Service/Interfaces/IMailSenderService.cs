9ad6b23486b1f8d16c7ca958747f34104ef21899
Denis Anisimov <den.anisimov.94@mail.ru>
2020-02-26 19:06:59 +0300

    Доработал интерфейсы

---
+++ MailSender-lib/Service/Interfaces/IMailSenderService.cs
@@ -0,0 +1,26 @@
+﻿using MailSender_lib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MailSender_lib.Service.Interfaces
+{
+    public interface IMailSenderService
+    {
+        IMailSender GetSender(Server Server);
+    }
+
+    public interface IMailSender
+    {
+        void Send(Mail Mail, Sender From, Recipient To);
+
+        void Send(Mail Message, Sender From, IEnumerable<Recipient> To);
+
+        Task SendAsync(Mail Mail, Sender From, Recipient To);
+
+        Task SendAsync(Mail Message, Sender From, IEnumerable<Recipient> To, CancellationToken Cancel = default);
+    }
+}
---
+++ MailSender-lib/Service/MailSchedulerTPL.cs
@@ -0,0 +1,63 @@
+﻿using MailSender_lib.Entities;
+using MailSender_lib.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MailSender_lib.Service
+{
+    public class MailSchedulerTPL : IMailScheduler
+    {
+        private readonly ISchedulerTasksStore _TasksStore;
+        private readonly IMailSenderService _MailSenderService;
+        private volatile CancellationTokenSource _ProcessTaskCancellation;
+
+        public MailSchedulerTPL(ISchedulerTasksStore TasksStore, IMailSenderService MailSenderService)
+        {
+            _TasksStore = TasksStore;
+            _MailSenderService = MailSenderService;
+        }
+
+        public void Start()
+        {
+            var cancellation = new CancellationTokenSource();
+            Interlocked.Exchange(ref _ProcessTaskCancellation, cancellation)?.Cancel();
+
+            var first_task = _TasksStore.GetAll()
+               .Where(task => task.Time > DateTime.Now)
+               .OrderBy(task => task.Time)
+               .FirstOrDefault();
+            if (first_task is null) return;
+
+            WaitTaskAsync(first_task, cancellation.Token);
+        }
+
+        private async void WaitTaskAsync(SchedulerTask task, CancellationToken Cancel)
+        {
+            Cancel.ThrowIfCancellationRequested();
+
+            var task_time = task.Time;
+            var delta = task_time.Subtract(DateTime.Now);
+
+            if (delta.TotalSeconds > 0)
+                await Task.Delay(delta, Cancel).ConfigureAwait(false);
+            Cancel.ThrowIfCancellationRequested();
+
+            await ExecuteTask(task, Cancel);
+            _TasksStore.Remove(task.Id);
+
+            await Task.Run(Start, Cancel);
+        }
+
+        private async Task ExecuteTask(SchedulerTask task, CancellationToken Cancel)
+        {
+            Cancel.ThrowIfCancellationRequested();
+
+            var sender = _MailSenderService.GetSender(task.Server);
+            await sender.SendAsync(task.Mail, task.Sender, task.Recipients.Recipients, Cancel);
+        }
+    }
+}
